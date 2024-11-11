fcac110622018696a0fbc00380bca4d685c73a47
rubencc <rubecanon@gmail.com>
2024-11-11 20:16:25 +0100

    EjercicioResuelto09

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -3,15 +3,19 @@ package com.campusdual.classroom;
 public class Exercise09 {
 
     public static void main(String[] args) {
-
+        doWhileLoop(5);
     }
 
-    //TODO ↓
+
     // Comprueba el funcionamiento del bucle do-while, que para cada interacción muestre el siguiente mensaje:
     // Entra con valor: X, sale con valor: X+1
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
     public static void doWhileLoop(int num) {
-
+        int numero = 0;
+        do {
+            System.out.println("Entra con valor: " + numero + ", sale con valor: " + (numero+1));
+            numero++;
+        } while (numero < num);
     }
 }
